bad552e1dbf294f3bf48f49a91f3295e0b699411
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-06-25 15:22:55 +0500

    add logging

--- messiah_bot.py
+++ messiah_bot.py
@@ -1,4 +1,6 @@
 __author__ = 'm_messiah'
+import logging
+import signal
 from requests import Session
 import tornado.ioloop
 import tornado.web
@@ -7,34 +9,43 @@ from base64 import b64decode
 
 from bot_token import BOT_TOKEN
 
+
+logging.basicConfig(filename="conversation.log", level=logging.INFO)
+
 # noinspection PyAbstractClass
 class Handler(tornado.web.RequestHandler):
-
-        def get(self):
-            try:
-                updates = api.get(URL + "getUpdates")
-                updates = updates.json()
-                if updates["ok"]:
-                    self.write("%s" % updates["result"])
-                else:
-                    print("FAIL: %s" % updates)
-            except Exception as e:
-                print("FAILURE: %s" % e)
-
         def post(self):
             try:
+                logging.debug("Got request: %s" % self.request.body)
                 update = tornado.escape.json_decode(self.request.body)
                 message = update['message']
                 text = message['text']
+
+                logging.info("MESSAGE\t%s (%s)\t%s" % (
+                    message['from']['username'],
+                    message['from']['id'],
+                    text))
+
                 if text[0] == '/':
                     command, *arguments = text.split(" ", 1)
-                    reply = COMMANDS.get(command, not_found)(arguments, message)
+                    reply = CMD.get(command, not_found)(arguments, message)
+
+                    logging.info("REPLY\t%s (%s)\t%s" % (
+                        message['from']['username'],
+                        message['from']['id'],
+                        reply
+                    ))
+
                     self.sendMessage(reply, command, message['from']['id'])
                 else:
+                    logging.info("COMMAND NOT FOUND\t%s (%s)" % (
+                        message['from']['username'],
+                        message['from']['id'],
+                    ))
                     self.sendMessage(not_found("", message), "/not_found",
                                      message['from']['id'])
             except Exception as e:
-                print("FAILURE: %s" % e)
+                logging.warning(str(e))
 
         def sendMessage(self, text, command, id):
             payload = {
@@ -66,7 +77,7 @@ def base64_decode(arguments, message):
         return "Can't decode it"
 
 URL = "https://api.telegram.org/bot%s/" % BOT_TOKEN
-COMMANDS = {
+CMD = {
     "/whoisyourdaddy": about,
     "/base64": base64_decode,
 }
@@ -77,11 +88,20 @@ application = tornado.web.Application([
 ])
 
 
+def signal_term_handler(signal, frame):
+    logging.error("Got SIGTERM. Quit.")
+    api.get(URL + "setWebhook?url=")
+    exit(0)
+
 
 if __name__ == '__main__':
-    set_hook = api.get(URL + "setWebhook?url=%s" % MyURL)
-    if set_hook.status_code != 200:
-        print("FAIL: %s" % set_hook.text)
-        exit(1)
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    signal.signal(signal.SIGTERM, signal_term_handler)
+    try:
+        set_hook = api.get(URL + "setWebhook?url=%s" % MyURL)
+        if set_hook.status_code != 200:
+            logging.error("Can't set hook: %s. Quit." % set_hook.text)
+            exit(1)
+        application.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        signal_term_handler(signal.SIGTERM, None)
